e17032cddd1b812eee77382d821eea4652217f22
drewhollar1 <106189949+dhollaractualize@users.noreply.github.com>
2023-11-21 00:48:09 -0500

    made some progress on sorting the csvs and installed a lot of setup

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,7 +5,11 @@ from glob import glob
 # Read single CSV file
 # --------------------------------------------------------------
 
+single_file_acc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy_MetaWear_2019-01-14T14.22.49.165_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
 
+single_file_gyr = pd.read_csv("../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv")
 # --------------------------------------------------------------
 # List all data in data/raw/MetaMotion
 # --------------------------------------------------------------
